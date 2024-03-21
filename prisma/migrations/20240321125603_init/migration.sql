0008ef469c520d6ddad66298dfb8e68bfba92723
kirankumal <kiran.kumal714@gmail.com>
2024-03-21 18:57:34 +0545

    :memo: Implement Swagger for API Documentation

---
+++ prisma/migrations/20240321125603_init/migration.sql
@@ -0,0 +1,27 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `imageUrl` VARCHAR(191) NULL DEFAULT 'https://res.cloudinary.com/dcy8a9bou/image/upload/v1707037177/IMG_378943359_ic_admin_user_default_z8bxwf.svg',
+    `phoneNumber` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Authentication` (
+    `id` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `role` ENUM('ADMIN', 'USER') NOT NULL DEFAULT 'USER',
+    `userId` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `Authentication_email_key`(`email`),
+    UNIQUE INDEX `Authentication_userId_key`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Authentication` ADD CONSTRAINT `Authentication_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
